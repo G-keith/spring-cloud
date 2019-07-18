7fed9fa3f19b31fc3feebcacb17132cab154133a
keith <G-keith>
2019-07-18 14:42:35 +0800

    cloud-eureka

---
+++ cloud-eureka/src/main/java/com/keith/eureka/CloudEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.keith.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *@author keith
+ *@date 2019/7/18
+ *@version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+
+}
